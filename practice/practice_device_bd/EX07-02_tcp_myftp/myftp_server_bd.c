f99fdb88c3e098df17db73079e4f7a4af224b85a
yedididi <iyeji366@gmail.com>
2025-02-03 11:12:33 +0900

    0702 first

--- practice/practice_device_bd/EX07-02_tcp_myftp/myftp_server_bd.c
+++ practice/practice_device_bd/EX07-02_tcp_myftp/myftp_server_bd.c
@@ -21,7 +21,11 @@ pid_t pid;
 int get_pwd(char *buf)
 {
 	/* Implement code */
-
+	buf = getcwd(0, sizeof(char) * 1024);
+	if (buf)
+		return (0);
+	else
+		return (1);
 
 }
 
@@ -34,20 +38,36 @@ void process_command(int sfd_client)
 
 	for(;;) {
 		/* Implement code: call read() */
+		len = read(sfd_client, rbuf, MAX_BUF-1);
+		if (len <= 0) {
+			close(sfd_client);
+			printf("[%d] closed\n", pid);
+			break;
+		}
 
+
+		else {
+			buf[len] = 0;
+			printf("[%d] received: %s\n", pid, buf);
+
+			/* echo back */
+			write(sfd_client, buf, len);
+		}
 
 		printf("[%d] received: %s", pid, rbuf);
 
 		if(strcmp(rbuf, "QUIT\r\n") == 0) {
 			/* Implement code: call write() */
-
+			write(sfd_client, "OK", 2);
 
 			return;
 		}
 		else if(strcmp(rbuf, "PWD\r\n") == 0) {
 			/* Implement code: call write() */
 
-
+			get_pwd(tbuf);
+			wbuf = strcat("ok", tbuf);
+			write(sfd_client, wbuf, strlen(wbuf));
 		}
 	}
 }
@@ -68,7 +88,11 @@ int main(int argc, char **argv)
 	printf("[%d] running %s\n", pid = getpid(), argv[0]);
 
 	/* Implement code: call socket() */
-
+	sfd_server = socket(AF_INET, SOCK_STREAM, 0);
+	if(sfd_server == -1) {
+		printf("[%d] error: %s (%d)\n", pid, strerror(errno), __LINE__);
+		return EXIT_FAILURE;
+	}
 
 	/* to prevent "Address already in use" error */
 	ret = setsockopt(sfd_server, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
@@ -78,23 +102,40 @@ int main(int argc, char **argv)
 	}
 
 	/* Implement code: call bind() */
-
+	memset(&addr_server, 0, sizeof(addr_server));
+	addr_server.sin_family = AF_INET;
+	addr_server.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr_server.sin_port = htons(SERVER_PORT);
+	ret = bind(sfd_server, (struct sockaddr *)&addr_server, sizeof(addr_server));
+	if(ret == -1) {
+		printf("[%d] error: %s (%d)\n", pid, strerror(errno), __LINE__);
+		return EXIT_FAILURE;
+	}
 
 	/* Implement code: call listen() */
-
+	ret = listen(sfd_server, LISTEN_BACKLOG);
+	if(ret == -1) {
+		printf("[%d] error: %s (%d)\n", pid, strerror(errno), __LINE__);
+		return EXIT_FAILURE;
+	}
 
 	for(;;) {
 		printf("[%d] waiting for client ...\n", pid);
-
+		
 		/* Implement code: call accept() */
-
+		addr_client_len = sizeof(addr_client);
+		sfd_client = accept(sfd_server, (struct sockaddr *)&addr_client, &addr_client_len);
+		if(sfd_client == -1) {
+			printf("[%d] error: %s (%d)\n", pid, strerror(errno), __LINE__);
+			return EXIT_FAILURE;
+		}
 
 		printf("[%d] connected\n", pid);
 
 		process_command(sfd_client);
 
 		/* Implement code: call close() */
-
+		close(sfd_client);
 
 		printf("[%d] closed\n", pid);
 	}
